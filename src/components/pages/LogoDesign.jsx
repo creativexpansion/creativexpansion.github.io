bed072dc69331fbf87b040839f9c10068e973be8
Davide Ciacco <davide.ciacco@neosperience.com>
2022-07-10 17:07:49 +0200

    mod pne, add mus logos

--- src/components/pages/LogoDesign.jsx
+++ src/components/pages/LogoDesign.jsx
@@ -6,6 +6,7 @@ import chi005 from "../../assets/images/portfolio/logos/chi005.png";
 import eli001 from "../../assets/images/portfolio/logos/eli001.png";
 import inn003 from "../../assets/images/portfolio/logos/inn003.png";
 import pne006 from "../../assets/images/portfolio/logos/pne006.png";
+import mus007 from "../../assets/images/portfolio/logos/mus007.png";
 import { titlePrefix } from "../utils";
 const StyledLogoDesign = styled.div`
   width: calc(100% + 34px);
@@ -32,11 +33,11 @@ const StyledLogoDesign = styled.div`
   }
 `;
 
-const logos = [eli001, aer002, inn003, can004, chi005, pne006];
+const logos = [eli001, aer002, inn003, can004, chi005, pne006, mus007];
 
 const LogoDesign = () => {
   useEffect(() => {
-    document.title = titlePrefix + 'Portfolio | Logo Design';
+    document.title = titlePrefix + "Portfolio | Logo Design";
   }, []);
   return (
     <StyledLogoDesign>
